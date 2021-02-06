4abe747b6c0877efc8f6d209f22d8488863df9b1
Zsolt Alfoldi <zsolt.alfoldi@blackbelt.hu>
2021-02-06 02:54:31 +0100

    .

--- gui.py
+++ gui.py
@@ -1,9 +1,4 @@
-# import Tkinter as tk
 from main import WordSearcher
-# import tkinter as tk
-
-# from tkinter import Tk, Canvas, Frame, BOTH, W
-
 from tkinter import *
 
 data = """almatjdatolyawisertm
@@ -73,113 +68,33 @@ class ExampleApp(Tk):
     def __init__(self):
         Tk.__init__(self)
 
-        # t.pack(fill=BOTH, expand=1)
         self._canvas = Canvas(self)
-        # canvas.create_line(0, 0, 200, 25)
-        # canvas.create_line(1, 10, 200, 10)
-        # canvas.create_line(1, 20, 200, 20)
-        # canvas.create_line(1, 30, 200, 30)
         self._padding = 0
         self._padding_step = 20
         self._col_padding = 0
         self._col_idx_list = []
         self._row_idx_list = []
-        # t_row_idx = 1
-        # t_cols_idx = 1
-        # self._yn = -1
-        # self._xn = -1
-        # canvas.create_line(10, 15, 10, 310)
-        # canvas.create_line(20, 15, 20, 310)
         for row_idx, row in enumerate(data.splitlines()):
 
-            # current_row = []
-            # canvas.create_line(10, row_idx + padding, 310, row_idx + padding)
-            # print(
-            #     10 +  len(row)  * 35
-            # )
             self._xn = 10 + len(row) * 35
             self._canvas.create_line(10, 10 + self._padding + row_idx, self._xn, 10 + self._padding + row_idx)
-
-            # print(
-            #     10 + padding + row_idx, 10 + (padding_step * len(data.splitlines())) + padding_step
-            # )
             self._col_idx_list = []
             for cols_idx, column in enumerate(row):
-
-                # canvas.create_line(cols_idx + col_padding, 15, cols_idx + col_padding, 310)
-                # t_row_idx = row_idx * 2 + 22
-                # t_cols_idx = cols_idx * 20 + 6
                 if not self._match:
-                    # canvas.create_line(10 + (col_padding + row_idx) * 1.2, 10, 10 + (col_padding + row_idx) * 1.2, 430)
-                    # canvas.create_line(10 + (col_padding) * 1.2, 10, 10 + (col_padding) * 1.2, 430)
                     self._yn = 10 + (22 + len(row) - 1 + (len(row) - 1) * self._padding_step)
-                    # canvas.create_line(10 + cols_idx * 35, 10, 10 + cols_idx * 35, 10 + (padding_step * len(data.splitlines())) + padding_step)
                     self._canvas.create_line(10 + cols_idx * 35, 10, 10 + cols_idx * 35, self._yn)
-                    # print(
-                    #     10 + cols_idx * 35, 10, 10 + cols_idx * 35, 430
-                    # )
-                    # print(
-                    #     10 + cols_idx * 35, "--->"
-                    # )
-                    # canvas.create_line(10 + t_row_idx, 15, 10 + t_cols_idx, 420)
-                # t_row_idx = padding * 2 + 22
-                # t_cols_idx = padding * 20 + 3.5
-                # t_row_idx = t_row_idx * 2 + 22
-                # t_cols_idx = t_cols_idx * 20 + 3.5
-                # cols_idx**
-                # print(
-                #     cols_idx + col_padding, "---->", cols_idx + col_padding
-                # )
-                # canvas.create_text(10+t_cols_idx, t_row_idx, anchor=W, font="Purisa", text="a")
-                # canvas.create_text(10 + t_cols_idx, t_row_idx + padding, anchor=W, font="Purisa", text=column)
-                # canvas.create_text(10 + t_cols_idx, 22+ row_idx + padding, anchor=W, font="Purisa", text=column)
-                # canvas.create_text(10 + t_cols_idx * 1.19, 22 + row_idx + padding, anchor=W, font="Purisa", text=column)
                 self._canvas.create_text(20 + cols_idx * 35, 22 + row_idx + self._padding, anchor=W, font="Purisa", text=column)
-                # print(
-                #     20 + cols_idx * 35, 22 + row_idx + padding
-                # )
                 self._col_idx_list.append([20 + cols_idx * 35, 22 + row_idx + self._padding])
-                # print(
-                #     "======>", 20 + cols_idx * 35, 22 + row_idx + padding, row_idx, padding, 22 + len(row) - 1 + (len(row) - 1) * padding_step
-                # )
-                # 22+  len(row) -1 + (len(row) -1) * padding_step
-                # (22 + (len(row) - 1) + (padding * (len(row) - 1)))
-                # canvas.create_text(8 + t_cols_idx, t_row_idx , anchor=W, font="Purisa", text=column)
-                # canvas.create_text(8 * 20 + 3.5, padding * 2 + 22, anchor=W, font="Purisa", text=column)
-                # canvas.create_text(t_row_idx, t_cols_idx, anchor=W, font="Purisa", text="a")
                 self._col_padding += 25
 
             self._match = True
             self._padding += self._padding_step
             self._row_idx_list.append(self._col_idx_list)
 
-        # canvas.create_line(10, 430, 455, 430)
-        # canvas.create_line(455, 10, 455, 430)
-        # 10 + ( 7 * 20 ) + 20  = 170 ==> 158 // 12
-        # 10 + ( 20 * 10 ) + 20 = 430 ==> 430
-        # yn = 10 + (padding_step * len(data.splitlines())) + padding_step
-        # yn = 10 + (22 + len(row) - 1 + (len(row) - 1) * padding_step)
-        # print(yn)
-        # yn = 158
         self._canvas.create_line(10, self._yn, self._xn, self._yn)
         self._canvas.create_line(self._xn, 10, self._xn, self._yn)
 
-        # canvas.create_text(20, 30, anchor=W, font="Purisa", text="a")
-
-        # t = SimpleTable(canvas, 10, 5)
-        # t.pack(side="top", fill="x")
-        # t.pack(fill=BOTH, expand=1)
-
         self._canvas.pack(fill=BOTH, expand=1)
-
-        # print(
-        #     row_idx_list
-        # )
-
-        # text_to_search = "görögdinnye"
-        # text_to_search = "ananász"
-
-        # canvas.create_line(x1 + 5, y1, x2 + 5, y2, width=10, fill='green', stipple='gray50')
 
     def draw_line(self, x1, y1, x2, y2):
         self._canvas.create_line(x1 + 5, y1, x2 + 5, y2, width=10, fill='green', stipple='gray50')
@@ -194,23 +109,7 @@ class TextBox(Text):
 
     def hello_button(self):
 
-        print("hellllllllllla")
-        print(
-            self._b.winfo_width()
-        )
-
-        # text_to_search_list = [
-        #     "görögdinnye",
-        #     "ananász",
-        #     "alma",
-        #     "szőlő",
-        #     "őszibarack",
-        #     "sárgadinnye",
-        #     "szeder",
-        #     "pocok",
-        # ]
         list_of_text = filter(None,self.get("1.0", END).splitlines())
-        # filter(None, list_of_text)
         for text in list_of_text:
             _ws = WordSearcher()
             _ws.gen_word_search_list(data)
@@ -240,7 +139,6 @@ class TextBox(Text):
 
         self._b = Button(master, text="Hello", command=self.hello_button)
 
-        # b.place(x=master._xn + 10, y=10, height=self._hei + 10, width=self._wid)
         self._b.place(x=master._xn + ((self._wid - 32) / 2), y=self._hei + 10)  # height=10, width=10)
 
         self.bind('<Return>', self.motion)
@@ -250,7 +148,6 @@ class TextBox(Text):
 
     def _init_textbox_text(self):
 
-        # _str = self.get("1.0", END)
         self.insert(INSERT, textbox_text)
         self._max = len(textbox_text.splitlines())
 
@@ -262,12 +159,6 @@ class TextBox(Text):
     _hei_step = 16
 
     def motion(self, event):
-        # T.set
-        # global hei
-        # global _max
-        # print(T.winfo_width)
-        # self._hei += 12.5
-        # self._hei += 16
         self._hei += self._hei_step
         _str = self.get("1.0", END)
         print(
@@ -284,11 +175,6 @@ class TextBox(Text):
         return
 
     def neg_motion(self, event):
-        # T.set
-        # global hei
-        # global _max
-        # print(T.winfo_width)
-
         _str = self.get("1.0", END)
         print(
             _str,
@@ -299,22 +185,12 @@ class TextBox(Text):
             self._max = len(_str.splitlines())
             self.place(x=self._master._xn + 10, y=10, height=self._hei, width=self._wid)
             self._b.place(x=self._master._xn + ((self._wid - 32) / 2), y=self._hei + 10)
-        # print("Mouse position: (%s %s)" % (event.x, event.y))
         return
 
 
 if __name__ == "__main__":
     app = ExampleApp()
-    # app.geometry('460x460')
-
     tb = TextBox(app)
-
-    # T = Text(app)  # , height=2, width=30, )  # command=spinbox1_callback)
-    # T.set
-    # hei = 50
     wid = 200
-    # T.place(x=app._xn + 10, y=10, height=hei, width=wid)
-
     app.geometry('%dx%d' % (app._xn + 10 + wid + 5, app._yn + 10))
-
     app.mainloop()
